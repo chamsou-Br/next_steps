e98fd955b5c8c93ac2095df2ba09e2ce7b3de1dd
chamsou-Br <jc_berkane@esi.dz>
2022-03-03 23:49:49 +0100

    add query

--- pages/coverLetter/informations.js
+++ pages/coverLetter/informations.js
@@ -9,13 +9,29 @@ import SkilllsCoverLetter from '../../components/SkilllsCoverLetter'
 import jobs from '../../data/jobs'
 import SoftwarSkillsCL from '../../components/SoftwarSkillsCL'
 import LastJobCL from '../../components/LastJobCL'
+import { useRouter } from 'next/router'
 
 function informations() {
+  const {query} = useRouter()
 
   return (
     <div className=' '>
         <PhaseCoverLetter data={2} />
-        <LastJobCL data={jobs} />
+        {query.step == "selectJob" ? (
+            <SelectJobCL data={jobs} />
+        ) : query.step == "lastJob" ? (
+          <LastJobCL data={jobs} />
+        ) : query.step == "skillsJob" ? (
+            <SkilllsCoverLetter data={jobs} />
+        ) : query.step == "skillsSoftwarJob" ? (
+            <SoftwarSkillsCL data={jobs} />
+        ) : query.step == "experience" ? (
+            <ExperienceCL data={jobs} />
+        ) : (
+          <div className=' h-screen flex justify-center' >
+              <h1 className=' m-auto font-bold text-xl' >404 page</h1>
+          </div>
+        ) }
         
 
 
@@ -23,4 +39,4 @@ function informations() {
   )
 }
 
-export default informations
+export default informations 
