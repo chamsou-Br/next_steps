009408b76c02bbdfe68959e521c6f69fc11a5716
chamsou-Br <jc_berkane@esi.dz>
2022-03-07 21:13:43 +0100

    add IT skills

--- data/skills.js
+++ data/skills.js
@@ -20,12 +20,12 @@ const skills = [
         list : ['Dental Assistant Skills','Taking Blood Pressure',"Taking Dental Radiographs","Taking Dental Impressions","Using Digital Radiography","Dental Hygienist Skills","Stress Management","Cleaning and bathing patients","Receiving Patients","Maintaining Medical Equipment",'Administering Injections',"Customer Service","Preparing Patients","Knowledge of Medical Terms","High energy","Maintaining patient confidentiality","Microsoft office"]
     },
     {
-        type : "",
-        list : [ ]
+        type : "Information Technology (IT) Jobs",
+        list : ["Command-line utilities","Cross-platform development","Cascading Style Sheets (CSS)","Git","Github","Graphical user interface design (UI)","HTML5","Java","JavaScript","C/C++","C#","Node.js development","Objective-C","PHP","Problem-solving skills","Python","web design","RESTful APIs ","Swift","User experience design (UX)","Test and debug code","Cloud Computing","Ruby","Artificial Intelligence","Big Data","Data Analysis","Critical Thinking","Data Visualization","Database Management","Data Manipulation","Data Wrangling","Data Tools","Data Mining","DevOps","Mathematics","Machine Learning Models","Linear Algebra","Statistics","Microsoft Office","NoSQL","MATLAB","MySQL","R","TensorFlow","Troubleshooting","Linux/Unix","Database Architecture","Optimization","SaaS","Search Engine Optimization (SEO)","XML","JSON","Open-source Software","Hardware Description Language","Hardware Tools","Adobe","Software Programming","Cisco","React", "Angular","Vuejs","React native","Flutter","Laravel","Django","Express js","Flask","Firebase","Kotlin","Tailwind css","Bootstrap Css"   ]
     },
     {
-        type : "",
-        list : [ ]
+        type : "Technical / Engineering Jobs",
+        list : ["Reading Comprehension","Data Analysis","Analytical Skills","Advanced Math","Design Skills","Architectural Rendering","Business Knowledge","Problem Solving","Visualization","Technical Vision","Aesthetic sensibility","Manual Dexterity ","Physical Strength","Mechanical Know-How","Troubleshooting Skills","Regulatory Compliance ","Electrical knowledge","Logical / “Thinking” Skills","Test Building Materials"]
     },
 
 ]
