6f33624c1bcf1a9ea1c7a72db3fe295ff8274b15
chamsou-Br <jc_berkane@esi.dz>
2022-03-07 20:09:53 +0100

    creativité skills

--- data/skills.js
+++ data/skills.js
@@ -4,8 +4,8 @@ const skills = [
         list : ["Communication","Business Correspondence","Public Speaking","Public Relations","Microsoft Office","Time & Billing","Organization","Planning","Problem-Solving ","Negotiating",'Critical Thinking','CEO Duties & Responsibilities','Environmental monitoring',"Circular economy","Strategic Skills","Team building",'Teamwork',"Research, Qualitative Skills","Innovation","Networking","Coordinating","Team Manager","Leadership","Microsoft Office","Logistics",]
     },
     {
-        type : "",
-        list : [ ]
+        type : "Creative / Media Industry Jobs",
+        list : ["Creativity","Innovation","Architecting Content","Marketing","Image management","Adobe Analytics","Microsoft office","Photoshop","Adobe InDesign",'Adobe Illustrator',"Adobe Creative Cloud","Adobe premier pro","Adobe after effect","Digital video cameras","Photography","Marking Color Breaks","Printing Layout","Infographics","Typography","Open-minded","Collaboration","Film Development","Digital Imaging",'Digital video cameras']
     },
     {
         type : "",
