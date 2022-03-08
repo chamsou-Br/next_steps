68fc2d327a8740303cf191f1cc0a48ba547a721d
chamsou-Br <jc_berkane@esi.dz>
2022-03-08 21:27:45 +0100

    add action name new cover letter

---
+++ Redux/ActionsName/Action_NewCL.js
@@ -0,0 +1,6 @@
+export const  INIT_NEWCL = "INIT_NEWCL" ;
+export const ADD_JOB = "ADD_JOB";
+export const ADD_LASTJOB = "ADD_LASTJOB";
+export const ADD_EXPERIENCE = "ADD_EXPERIENCE";
+export const ADD_SOFTWARSKILLS = "ADD_SOFTWARSKILLS"
+export const ADD_SKILLS = "ADD_SKILLS"
