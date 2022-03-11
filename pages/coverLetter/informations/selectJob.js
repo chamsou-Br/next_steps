116de5328ad1a1f9eb008f5c2d5b83ec7448d1ad
chamsou-Br <jc_berkane@esi.dz>
2022-03-11 15:40:48 +0100

    add select job  route

---
+++ pages/coverLetter/informations/selectJob.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import PhaseCoverLetter from '../../components/CoverLetter/PhaseCoverLetter'
+import jobs from '../../data/jobs'
+import SelectJobCL from '../../../components/SelectJobCL'
+
+function selectJob() {
+  
+
+  return (
+    <div className=' '>
+        <PhaseCoverLetter data={2} />
+        <SelectJobCL data={jobs} />        
+    </div>
+  )
+}
+
+
+export default selectJob 
