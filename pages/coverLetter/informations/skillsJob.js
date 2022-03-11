50f38771661d2d25467d960c9fda5b4f16eb0f27
chamsou-Br <jc_berkane@esi.dz>
2022-03-11 15:41:38 +0100

    add select skills  route

---
+++ pages/coverLetter/informations/skillsJob.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import PhaseCoverLetter from '../../components/CoverLetter/PhaseCoverLetter'
+import jobs from '../../data/jobs'
+import SkilllsCoverLetter from '../../../components/SkilllsCoverLetter'
+
+function skillsJob() {
+  
+
+  return (
+    <div className=' '>
+        <PhaseCoverLetter data={2} />
+        <SkilllsCoverLetter data={jobs} />        
+    </div>
+  )
+}
+
+
+export default skillsJob 
