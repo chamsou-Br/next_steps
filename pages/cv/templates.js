60123cf04ca5a5bede8d2bbfe44a23ce3f045879
chamsou-Br <jc_berkane@esi.dz>
2022-03-20 16:43:26 +0100

    ADD ACTION_REDUX OF NEW CV

--- pages/cv/templates.js
+++ pages/cv/templates.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PhaseCoverLetter from '../../components/CoverLetter/PhaseCoverLetter'
+import PhaseStep from '../../components/shared/PhaseStep'
 import SwiperCoverLetter from '../../components/shared/swiperTemplates'
 
 const data = [
@@ -13,7 +13,7 @@ const data = [
 function templates() {
   return (
     <div className='container-snap min-h-screen'>
-            <PhaseCoverLetter data={1} />
+            <PhaseStep data={1} />
             <h1 className=' mt-8 mb-12 text-center text-blue-500 font-bold tracking-wider lg:text-3xl md:text-xl text-lg'>Lat's start with  your Template</h1>
             <div>
                 <SwiperCoverLetter route="/cv/informations/personnalInformation" data={data} />
